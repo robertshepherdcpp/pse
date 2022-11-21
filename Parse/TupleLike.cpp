6752fe542fbe5218a5080c6feb5f689ea7079b15
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2022-11-21 17:25:55 +0000

    improve TupleLike

--- Parse/TupleLike.cpp
+++ Parse/TupleLike.cpp
@@ -53,6 +53,58 @@ namespace pse
 	* auto make_TupleLike(T t, TupleLike)
 	*/
 
+	namespace global_elements_tupleLike
+	{
+		auto count = 0;
+	}
+
+	template<typename T, typename... Ts>
+	auto GetParameterPackIndex(TupleLike<T, Ts...> tuple, auto index)
+	{
+		if(global_elements_tupleLike::count == index)
+		{
+			return tuple.first;
+		}
+		else 
+		{
+			// we have to increment global_elements_tupleLike like this:
+			global_elements_tupleLike::count += 1;
+			return GetParameterPackIndex(TupleLike.seconds, index);
+		}
+	}
+
+	template<typename T>
+	auto GetParameterPackIndex(TupleLike<T> tuple, auto index
+	{
+		return tuple.first; // no exception hanling currently, if wrong input from the user it is the user's fault.
+	}
+    
+	template<typename T, typename... Ts>
+	auto get_tuple_elements(TupleLike<T, Ts...>& tuple)
+	{
+		if(tuple.index == 0)
+		{
+		return T;
+		}
+		else
+		{
+			/*
+			Index is to show that becuase we are going to use this function like this:
+			get_tuple_elements(tuple)...
+			We are going to need to know how deep we need to go in the TupleLike object. For instance:
+
+			tuple.first;
+			tuple.seconds;
+			tuple.seconds.first;
+			tuple.seconds.seconds.first;
+
+			Extra so we need a count to track to get all of the elements in the TupleLike object.
+			*/
+			tuple.index++;
+			return GetParameterPackIndex(tuple, tuple.index);
+		}
+	}
+
 	template<typename T, typename... Ts>
 	auto GetType(T t, Ts... ts)
 	{
@@ -64,6 +116,32 @@ namespace pse
 	{
 		return Tuple<T, A, B>(t, tuple, b);
 	}
+
+
+    /*
+	Have hit a conumdrum with make_TupleLike becuase i can't define it like this:
+
+	template<typename T, typename B, typename... Ts, typename... Tstwo>
+	auto make_TupleLike(T t, Ts... ts, TupleLike<Tstwo...> tuple, B b)
+	{
+		return make_TupleLike(t, ts..., tuple.first, tuple.seconds, b);
+	}
+
+	and then have the base case as:
+
+	template<typename T, typename... Ts, typename Ttwo>
+	auto make_TupleLike(T t, Ts... ts, TupleLike<Ttwo> tuple, B b)
+	{
+		return TupleLike<T, Ts..., decltype(tuple.first), B>(t, ts..., tuple.first, b);
+	}
+
+	but is impossible becuase i am not allowed two parameter packs together because
+	which one they would go in would be ambiguous.
+
+	so i am not sure what to do.
+
+	I could have one parameter pack. Yes that is what i will do. No i cannot do that because i have got to have that tuple variadic args.
+	*/
 
 	template<typename T, typename... Ts>
 	struct TupleLike
@@ -78,6 +156,7 @@ namespace pse
 
 		T first;
 		TupleLike<Ts...> seconds;
+		int index = 0;
 
 	};
 
@@ -85,5 +164,6 @@ namespace pse
 	struct TupleLike<T>
 	{
 		T first;
+		int index = 0;
 	};
 } // namespace pse
