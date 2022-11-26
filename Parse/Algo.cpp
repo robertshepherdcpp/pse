e0eaec2e0cebee0ffb551ccef50b23031f846884
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2022-11-26 18:03:54 +0000

    `add the Algo.cpp header file and Range.cpp header file`

---
+++ Parse/Algo.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace pse
+{
+	inline namespace algorithms
+	{
+	    inline auto difference(int a, int b) -> int
+		{
+			return b - a;
+		}
+	} // namespace algorithms
+} // namespace pse
---
+++ Parse/Range.cpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include"Initializer_List.cpp" // pse::Initializer_List
+#include"Algo.cpp" // pse::algorithms
+
+namespace pse
+{
+	inline namespace Ranges
+	{
+		template<typename T>
+		struct Range
+		{
+			Range(Initializer_List<T>& init)
+			{
+				auto Size = init.size();
+				arr = new T[Size];
+				for (int i = 0; i < init.size(); i++)
+				{
+					arr[i] = init[i];
+				}
+			}
+
+			inline auto Take(int prefix, int postfix)
+			{
+				auto diff = algorithms::difference(postfix - prefix);
+				T temp[size]{};
+				for (int i = 0; i < size; i++)
+				{
+					temp[i] = arr[i];
+				}
+				size = diff;
+				arr = new T[diff];
+				for (int i = prefix; i < postfix; i++)
+				{
+					arr[i - prefix] = temp[i];
+				}
+			}
+
+			inline static int size = 1;
+			T arr[1];
+		};
+	} // namespace Ranges
+} // namespace pse
