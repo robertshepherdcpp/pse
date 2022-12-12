4ea219e6efd234d6f9b975d75715b70acda3a2b3
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2022-12-12 18:56:33 +0000

    Add a `code` data member to `pse::ErrorCode`, so that we don't have to store a string(Expensive!) we can just store a code/int.

--- Parse/ErrorCode.cpp
+++ Parse/ErrorCode.cpp
@@ -6,10 +6,12 @@ namespace pse
 {
 	struct ErrorCode
 	{
+		int code = 0;
 		StringList m_str{};
 		auto what() { return m_str; }
 
 		ErrorCode(StringList str) { m_str = str; }
 		ErrorCode() {}
+		ErrorCode(int x) { code = x; }
 	};
 } // namespace pse
