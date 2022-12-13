d6c4221eb1446dd5e893a7ff3f83ef7990d6541d
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2022-12-13 21:21:10 +0000

    Add a better return type to `pse::CodeMeaning` and add error code `054`

--- Parse/CodeMeaning.cpp
+++ Parse/CodeMeaning.cpp
@@ -5,15 +5,16 @@ namespace pse
 	// this functions takes an error code from pse::ErrorCode and outputs it's meaning.
 	// Everything new will also be added to the README.md part of the pse library
 
-	auto CodeMeaning(int x) -> decltype(auto)
+	const char* CodeMeaning(int x)
 	{
 		if (x == 026)
 		{
 			return "Out of Bounds Access.\n";
 		}
-		else if (false/*Other code number*/) // don't have another code number yet.
+		else if (054) // don't have another code number yet.
 		{
 			// No implementation yet.
+			return "Invalid Type";
 		}
 	}
 } // namespace pse
