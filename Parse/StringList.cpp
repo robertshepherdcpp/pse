6780f70198388534b7912740835762bc6b2d7222
unknown <shahh8138@gmail.com>
2022-12-15 10:37:25 +0530

    separated header files

---
+++ Parse/StringList.cpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include"ArrCpy.cpp"
+
+namespace pse
+{
+	// FIXME: Give better implemenatation of String
+	template<auto T>
+     struct StringList
+	   {
+		template<int Size>
+		StringList(char(&arr)[Size])
+		{
+				char temp[size];
+				if (Size > size)
+				{
+				for (int i = 0; i < size; i++)
+				{
+					temp[i] = c_arr[i];
+				}
+
+				c_arr = new char[Size];
+				size = Size;
+			}
+			for (int i = 0; i < size && i < size; i++)
+			{
+				c_arr[i] = arr[i];
+			}
+		}
+
+		StringList() {/*Needs implementing.*/ }
+
+		auto print()
+		{
+			for(int i = 0; i < size; i++)
+			{
+				
+				// std::cout << c_arr[i];
+				if (i != size - 1)
+				{
+					//std::cout << "";
+				}
+			}
+		}
+
+		int size = 1;
+		char c_arr[1];
+	};
+}
