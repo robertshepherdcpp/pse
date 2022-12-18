10b03f845e974c1962d70a0bde78f186c7c693fe
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2022-12-18 14:05:31 +0000

    Add a second overload of `Most` and change from accepting a NTTP as auto rather than an int.

--- Parse/Most.cpp
+++ Parse/Most.cpp
@@ -2,9 +2,15 @@
 
 namespace pse
 {
-	template<int A, int B>
+	template<auto A, auto B>
 	constexpr auto Most()
 	{
 		return A < B;
+	}
+
+	template<typename A, typename B>
+	constexpr auto Most(A a, B b)
+	{
+		return a < b;
 	}
 }
