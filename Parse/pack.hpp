50cae6583568204c18eea0b206cc92f533ae297c
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-01-29 11:03:17 +0000

    add `pack.hpp`. Aslo in one of my other libraries, `mlib`.

---
+++ Parse/pack.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+namespace pse
+{
+    template <typename T>
+    struct type_holder {};
+
+    template <auto I>
+    struct index_pack {};
+
+    template <typename... Ts>
+    struct type_pack {
+        static constexpr auto pack_size = (sizeof(Ts) + ... + 0);
+
+        using start_type = decltype(mlib::get_nth_element<0>((Ts{})...));
+        using end_type = decltype(mlib::get_nth_element<(pack_size - (pack_size - 1)) / 2>((Ts{})...));
+
+        static constexpr auto start_value =
+            decltype(mlib::get_nth_element<0>((Ts{})...)){};
+        static constexpr auto end_value =
+            decltype(mlib::get_nth_element<(pack_size - (pack_size - 1)) / 2>((Ts{})...)){};
+        constexpr auto begin() { return start_value; }
+        constexpr auto end() { return end_value; }
+
+        template <auto I>
+        constexpr auto operator[](mlib::index_pack<I>) {
+            return mlib::get_nth_element<I>((Ts{})...);
+        }
+
+        template <auto T>
+        constexpr auto get() {
+            return mlib::get_nth_element<T>((Ts{})...);
+        }
+    };
+
+    template <auto... Ts>
+    struct value_pack {
+        static constexpr auto begin_value = mlib::get_nth_element<0>(Ts...);
+        static constexpr auto end_value =
+            mlib::get_nth_element<sizeof...(Ts) - 1>(Ts...);
+        static constexpr auto size_value = sizeof...(Ts) - 1;
+
+        constexpr auto size() { return size_value; }
+
+        constexpr auto begin() { return begin_value; }
+        constexpr auto end() { return end_value; };
+
+        template <auto I>
+        constexpr auto operator[](index_pack<I>) {
+            return mlib::get_nth_element<I>(Ts...);
+        }
+
+        template<auto... i>
+        constexpr auto for_each_helper(auto& lambda, std::index_sequence<i...>)
+        {
+            return mlib::value_pack<lambda(mlib::get_nth_element<i>(Ts...))...>{};
+        }
+
+        constexpr auto for_each(auto& lambda)
+        {
+            return for_each_helper(lambda, std::make_index_sequence < sizeof...(Ts)>{});
+        }
+
+        template <auto I>
+        constexpr auto get() {
+            return mlib::get_nth_element<I>(Ts...);
+        }
+
+        value_pack() { /*Does Nothing!*/
+        }
+    };
+} // namespace pse
